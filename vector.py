67e81049e88f22f890dced9095681dfd1975289c
ILoveNumericalMethods <98276657+ILoveNumericalMethods@users.noreply.github.com>
2022-12-06 12:34:55 +0500

    Update vector.py
    Переписано на numpy, осталось реализовать визуализацию

--- vector.py
+++ vector.py
@@ -1,60 +1,17 @@
+import numpy
 
 class Vector:
-    """
-    Тип данных, описывающий вектор.
-    x_end - x кооридината конца вектора
-    y_end - y кооридината конца вектора
-    x_begin - x кооридината начала вектора
-    y_begin - y кооридината начала вектора
-    """
-
-    def __init__ (self, x_begin, y_begin, x_end, y_end):
-        """
-        Конструктор
-        """
-        self.x_begin = x_begin
-        self.y_begin = y_begin
-        self.x_end = x_end
-        self.y_end = y_end
+    """    Тип данных, описывающий вектор.    vector - координаты вектора    """    def init (self, x, y):
+        """        Конструктор        """        self.vector = numpy.array([x, y])
 
     def __add__(self, other):
-        vector = Vector(0, 0, 0, 0)
 
-        vector.x_begin = self.x_begin
-        vector.y_begin = self.y_begin
-        vector.x_end = self.x_end + other.x_end - other.x_begin
-        vector.x_end = self.y_end + other.y_end - other.y_begin
-
-        return vector
-
-    def __add__(self, other):
-        vector = Vector(0, 0, 0, 0)
-
-        vector.x_begin = self.x_begin
-        vector.y_begin = self.y_begin
-        vector.x_end = self.x_end + other.x_end - other.x_begin
-        vector.x_end = self.y_end + other.y_end - other.y_begin
-
-        return vector
+        return self.vector + other.vector
 
     def __mul__(self, other):
-        vector = Vector(0, 0, 0, 0)
 
-        vector.x_begin = self.x_begin
-        vector.y_begin = self.y_begin
-        vector.x_end = self.x_end * other
-        vector.x_end = self.y_end * other
+        return numpy.dot(self.vector, other.vector)
 
-        return vector
-
-
-    def dot_product(self, vector):
-        """
-        скаларное поизведение
-        """
-        return (self.x_end - self.x_begin) * (vector.x_end - vector.x_begin) + (self.y_end - self.y_begin) * (vector.y_end - vector.y_begin)
 
     def draw (self):
-        """
-        дописать отрисовку вектора
-        """
+        """        дописать отрисовку вектора        """
