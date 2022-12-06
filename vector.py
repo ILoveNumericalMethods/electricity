d2f386bb4f7bd6ad5f7019fa2cac0b18c00a103c
Alexandra24032004 <74011794+Alexandra24032004@users.noreply.github.com>
2022-12-07 01:15:43 +0300

    Update vector.py
    реализовано умножение на скаляр

--- vector.py
+++ vector.py
@@ -1,16 +1,28 @@
 import numpy
 
 class Vector:
-    """    Тип данных, описывающий вектор.    vector - координаты вектора    """    def init (self, x, y):
-        """        Конструктор        """        self.vector = numpy.array([x, y])
+    """
+    Тип данных, описывающий вектор.
+    vector - координаты вектора
+    """
+    def __init__(self, x, y):
+        """
+        Конструктор
+        """
+        self.vector = numpy.array([x, y])
 
     def __add__(self, other):
 
-        return self.vector + other.vector
+        ans = self.vector + other.vector
+        return Vector(ans[0], ans[1])
 
     def __mul__(self, other):
 
         return numpy.dot(self.vector, other.vector)
+
+    def __mod__(self, other):
+
+        return Vector(self.vector[0] * other, self.vector[1] * other)
 
 
     def draw (self):
