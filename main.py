d1020ad9e12b7ea7f185be52f2748ca07629c9b9
Alya_Panshina <alya.panshina.04@mail.ru>
2022-12-15 01:05:32 +0300

    now the function is able to get text from a string

--- main.py
+++ main.py
@@ -3,35 +3,29 @@ import tkinter
 from tkinter.filedialog import *
 from tkinter import ttk
 
-def start_execution():
-    print("start")
-def show_message():
-    label["text"] = entry.get()     # получаем введенный текст
+def show_message(): #функция считывает введённую формулу и запускает функцию создания объекта
+    print(entry.get())     # получаем введенный текст
 
-def main():
-    """Главная функция главного модуля.
-    Создаёт объекты графического дизайна библиотеки tkinter: окно, холст, фрейм с кнопками, кнопки.
-    """
-    root = tkinter.Tk()
-    # пространство отображается на холсте типа Canvas
-    space = tkinter.Canvas(root, width=1200, height=600, bg="black")
-    space.pack(side=tkinter.BOTTOM)
-    # верхняя панель с кнопками
-    frame = tkinter.Frame(root)
-    frame.pack(side=tkinter.RIGHT)
+#создаём окно
+root = tkinter.Tk()
+root.title("METANIT.COM")
+root.geometry("1200x600")
 
-    start_button = tkinter.Button(frame, text="Start", command=start_execution, width=10)
-    start_button.pack(side=tkinter.LEFT)
+#создание холста
+space = tkinter.Canvas(root, width=1200, height=560, bg="black")
+space.pack(side=tkinter.BOTTOM)
 
-    entryExample = tkinter.Entry(root,
-                            width=180)
+# верхняя панель с кнопками
+frame = tkinter.Frame(root)
+frame.pack(side=tkinter.RIGHT)
 
-    entryExample.pack(side=tkinter.LEFT,
-                      padx=10)
+#окно для ввода
+entry = ttk.Entry()
+entry = tkinter.Entry(root, width=180)
+entry.pack(anchor=NW, padx=6, pady=8)
 
-    root.mainloop()
+#кнопка Добавить объект
+button = tkinter.Button(frame, text="Добавить объект", command=show_message, width=20)
+button.pack(side=tkinter.LEFT)
 
-
-
-if __name__ == '__main__':
-    main()
+root.mainloop()
