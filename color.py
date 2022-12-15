7390556ed92422564ba0c798cae3c9be94d67a72
Alya_Panshina <alya.panshina.04@mail.ru>
2022-12-15 22:28:53 +0300

    added color and model functions

---
+++ color.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+c1='#00FFFF' #blue
+c2='#EE82EE' #purple
+
+def fadeColor(c1,c2,mix=0): #создаёт линейную интерполяцию от цвета с1 до цвета с2
+    assert len(c1)==len(c2)
+    assert mix>=0 and mix<=1, 'mix='+str(mix)
+    rgb1=np.array([int(c1[ii:ii+2],16) for ii in range(1,len(c1),2)])
+    rgb2=np.array([int(c2[ii:ii+2],16) for ii in range(1,len(c2),2)])
+    rgb=((1-mix)*rgb1+mix*rgb2).astype(int)
+    c='#'+('{:}'*3).format(*[hex(a)[2:].zfill(2) for a in rgb])
+    return c
+
+def coloring (arr):
+    """
+    создаёт массив с цветами градиента из массива с интенсивностью параметра
+
+    :param arr: двумерный массив с интентсивностью параметра
+    :return coloured_arr: трёхмерный массив, раскрашенный градиентом
+    """
+    coloured_arr = np.zeros((arr.shape[0], arr.shape[1], 3), dtype=int)
+    for x in range (arr.shape[0]):
+        for y in range (arr.shape[1]):
+            if arr[x][y]>-1:
+                color1 = fadeColor(c1,c2, arr[x][y]/1000) #цвет пикселя
+                r = int(color1[1:3], 16)
+                g = int(color1[3:5], 16)
+                bl = int(color1[5::], 16)
+                coloured_arr[x][y][0] = r
+                coloured_arr[x][y][1] = g
+                coloured_arr[x][y][2] = bl
+            else:
+                coloured_arr[x][y][0] = 0
+                coloured_arr[x][y][1] = 0
+                coloured_arr[x][y][2] = 0
+    return (coloured_arr)
--- converting_formulas.py
+++ converting_formulas.py
@@ -1,1 +1,17 @@
-
+from model import *
+from color import *
+objects = []
+arr_potential = []
+k=0
+def converting(string_formula):
+    """
+    Получает на вход формулу объекта, создаёт объект и добавляет его в список объектов
+    :param string_formula:
+    :return:
+    """
+    global objects
+    global arr_potential
+    global k
+    k = k+1
+    objects.append(k) #добавление объекта в список
+    arr_potential = coloring(calculating(objects))
