dc9e404f84960fa649317db58813601b259da811
ILoveNumericalMethods <98276657+ILoveNumericalMethods@users.noreply.github.com>
2022-12-15 23:38:27 +0300

    Класс шаров

---
+++ Circle.py
@@ -0,0 +1,19 @@
+from math import pi
+
+class Circle:
+    """
+    Класс окружности
+    charge - заряд
+    x - координата по x
+    y - координата по y
+    radius - радиус
+    """
+
+    def __init__ (self, x, y, radius):
+        """
+        Конструктор
+        """
+        self.radius = radius
+        self.charge = pi * self.radius ** 2
+        self.x = x
+        self.y = y
