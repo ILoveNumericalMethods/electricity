09f9f63238483b9b23fdd54276d2287e8358f935
ILoveNumericalMethods <98276657+ILoveNumericalMethods@users.noreply.github.com>
2022-12-19 09:29:41 +0300

    fixing bugs

--- idetify.py
+++ idetify.py
@@ -1,4 +1,8 @@
 def is_in_figure(expression, x, y, object_x, object_y):
+    """
+    cheking if the point fit expression
+    """
     x -= object_x
     y -= object_y
     return eval(expression, {"x" : x, "y" : y})
+
