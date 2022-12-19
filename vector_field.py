543de73a9d6a32d6224ad0ab9807138e78420218
Alexandra24032004 <74011794+Alexandra24032004@users.noreply.github.com>
2022-12-19 10:01:08 +0300

    clean

--- vector_field.py
+++ vector_field.py
@@ -1,9 +1,8 @@
-import  pygame
+import pygame
 
 def draw_vector_field (scalar_field, screen, screen_x, screen_y):
    """
    calculating vector field
-
    GREED_STEP = distace between vectors
    """
    GREED_STEP = 10
@@ -11,12 +10,12 @@ def draw_vector_field (scalar_field, screen, screen_x, screen_y):
    BLACK = (0, 0, 0)
    CYAN = (0, 255, 255)
 
-   pygame.draw.rect(screen, BLACK, pygame.Rect(screen_x[0], screen_y[0], \ 
+   pygame.draw.rect(screen, BLACK, pygame.Rect(screen_x[0], screen_y[0], \
                    screen_x[1] - screen_x[0], screen_y[1] - screen_y[0]))
 
    for y in range(1, screen_y[1] - screen_y[0], GREED_STEP):
         for x in range(1, screen_x[1] - screen_x[0], GREED_STEP):
-            if (scalar_field[y][x + 1] != -1 and scalar_field[y][x - 1] != -1 and scalar_field[y + 1][x] != -1 and scalar_field[y - 1][x] != -1):
+            if scalar_field[y][x + 1] != -1 and scalar_field[y][x - 1] != -1 and scalar_field[y + 1][x] != -1 and scalar_field[y - 1][x] != -1:
                 vector_x = (scalar_field[y][x + 1] - scalar_field[y][x - 1]) / 2
                 vector_y = (scalar_field[y + 1][x] - scalar_field[y - 1][x]) / 2
 
